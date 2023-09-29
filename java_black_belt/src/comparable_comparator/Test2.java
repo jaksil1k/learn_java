da64a21251a8f7f361cb27149cc24f5739925207
Zhaksylyk <nartai53@gmai.com>
2023-09-29 14:46:35 +0600

    creating comparator by salary

--- java_black_belt/src/comparable_comparator/Test2.java
+++ java_black_belt/src/comparable_comparator/Test2.java
@@ -9,21 +9,21 @@ public class Test2 {
         Employee emp2 = new Employee(15, "Ivan", "Petrov", 6542);
         Employee emp3 = new Employee(123, "Ivan", "Sidorov", 8542);
 
-        //have to implement comparable to sort
-        Arrays.sort(new Employee[]{emp1, emp2, emp3});
+//        Arrays.sort(new Employee[]{emp1, emp2, emp3});
 
         list.add(emp1);
         list.add(emp2);
         list.add(emp3);
 
+        //have to implement comparable to sort
         System.out.println("Before sorting\n" + list);
-        Collections.sort(list);
+        Collections.sort(list, new SalaryComparator());
         System.out.println("After sorting\n" + list);
 
     }
 }
 
-class Employee implements Comparable<Employee> {
+class Employee /*implements Comparable<Employee>*/ {
     int id;
     String name;
     String surname;
@@ -46,10 +46,19 @@ class Employee implements Comparable<Employee> {
                 '}';
     }
 
+//    @Override
+//    public int compareTo(Employee emp) {
+////        return Integer.compare(this.id, emp.id);
+////        return this.id - emp.id;
+//        return this.name.compareTo(emp.name);
+//    }
+}
+
+class SalaryComparator implements Comparator<Employee> {
+
     @Override
-    public int compareTo(Employee emp) {
-//        return Integer.compare(this.id, emp.id);
-//        return this.id - emp.id;
-        return this.name.compareTo(emp.name);
+    public int compare(Employee em1, Employee emp2) {
+        return Integer.compare(em1.salary, emp2.salary);
     }
 }
+
