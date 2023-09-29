647b0dcbaa39e91a5c68f3584a22470986bfcfd3
Zhaksylyk <nartai53@gmai.com>
2023-09-29 16:00:25 +0600

    generics: type erasure

--- java_black_belt/src/generics/ParametrizedMethod.java
+++ java_black_belt/src/generics/ParametrizedMethod.java
@@ -4,14 +4,39 @@ import java.util.*;
 
 public class ParametrizedMethod {
     public static void main(String[] args) {
+        //after compilation, jvm makes type erasure to maintain backward compatibility
+        //in other word after compilation all objects casts to Object
         List<Integer> list = new ArrayList<>(Arrays.asList(10, 20, 5));
+        //List list = new ArrayList();
         int a = GenMethod.getSecondElement(list);
+        //int a = (Integer)GenMethod.getSecondElement(list);
         System.out.println(a);
+    }
+
+    //compilation error here is evidence of type erasure
+    public void abc(Info<String> info) {
+        String s = info.getValue();
+    }
+    public void abc(Info<Integer> info) {
+        Integer i = info.getValue();
     }
 }
 
-class GenMethod{
+class GenMethod {
     public static <T> T getSecondElement(List<T> list) {
         return list.get(1);
+    }
+}
+
+class Parent {
+    public void abc(Info<String> info) {
+        String s = info.getValue();
+    }
+}
+
+class Child extends Parent {
+    //compiler forbids it
+    public void abc(Info<Integer> info) {
+        Integer i1 = info.getValue();
     }
 }
