26f9a6bc7531f2ee63c18c438ba0349a7781f780
Zhaksylyk <nartai53@gmai.com>
2023-10-19 14:21:45 +0600

    collection: priorityQueue

---
+++ java_black_belt/src/collection/queue_interface/PriorityQueueEx1.java
@@ -0,0 +1,17 @@
+package collection.queue_interface;
+
+import java.util.PriorityQueue;
+
+public class PriorityQueueEx1 {
+    public static void main(String[] args) {
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
+        priorityQueue.add(4);
+        priorityQueue.add(1);
+        priorityQueue.add(7);
+        priorityQueue.add(10);
+        priorityQueue.add(8);
+
+        System.out.println(priorityQueue.peek());
+        priorityQueue.remove();
+    }
+}
---
+++ java_black_belt/src/collection/queue_interface/PriorityQueueEx2.java
@@ -0,0 +1,58 @@
+package collection.queue_interface;
+
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.PriorityQueue;
+
+public class PriorityQueueEx2 {
+    public static void main(String[] args) {
+        Student st1 = new Student("Zaur", 5);
+        Student st2 = new Student("Misha", 1);
+        Student st3 = new Student("Igor", 2);
+        Student st4 = new Student("Marina", 3);
+        Student st5 = new Student("Olya", 4);
+        PriorityQueue<Student> priorityQueue = new PriorityQueue<>();
+        priorityQueue.addAll(Arrays.asList(st1, st2, st3, st4, st5));
+        System.out.println(priorityQueue.poll());
+        System.out.println(priorityQueue.poll());
+        System.out.println(priorityQueue.poll());
+        System.out.println(priorityQueue.poll());
+        System.out.println(priorityQueue.poll());
+    }
+}
+
+class Student implements Comparable<Student>{
+    String name;
+    int course;
+
+    public Student(String name, int course) {
+        this.name = name;
+        this.course = course;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", course=" + course +
+                '}';
+    }
+
+    @Override
+    public int compareTo(Student st) {
+        return course - st.course;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return course == student.course;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(course);
+    }
+}
