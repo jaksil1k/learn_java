6f7409fc3beb2f88e91ace2a6da6f3537f37774f
Zhaksylyk <nartai53@gmai.com>
2023-11-23 16:44:21 +0600

    nested-class:inner nested class

---
+++ java_black_belt/src/nested_classes/inner_class/Car.java
@@ -0,0 +1,52 @@
+package nested_classes.inner_class;
+
+public class Car {
+    String color;
+    int doorCount;
+
+    Engine engine;
+
+    public Car(String color, int doorCount) {
+        this.color = color;
+        this.doorCount = doorCount;
+//        this.engine = this.new Engine(horsePower);
+    }
+
+    public void setEngine(Engine engine) {
+        this.engine = engine;
+    }
+
+    @Override
+    public String toString() {
+        return "MyCar{" +
+                "color='" + color + '\'' +
+                ", doorCount=" + doorCount +
+                ", engine=" + engine +
+                '}';
+    }
+
+    public class Engine {
+        int horsePower;
+
+        public Engine(int horsePower) {
+            this.horsePower = horsePower;
+        }
+
+        @Override
+        public String toString() {
+            return "MyEngine{" +
+                    "horsePower=" + horsePower +
+                    '}';
+        }
+    }
+}
+
+class Test {
+    public static void main(String[] args) {
+        Car car = new Car("black", 4);
+        car.setEngine(car.new Engine(150));
+        System.out.println(car.engine);
+        System.out.println(car);
+    }
+}
+
