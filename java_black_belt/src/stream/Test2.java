6e015fcdf1f3e4fbc8aab0472e10ac5ed5848a16
Zhaksylyk <nartai53@gmai.com>
2023-11-27 10:54:24 +0600

    stream api: method filter

---
+++ java_black_belt/src/stream/Test2.java
@@ -0,0 +1,52 @@
+package stream;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Test2 {
+    public static void main(String[] args) {
+        Student st1 = new Student("Ivan", 'm' , 22, 3, 8.3);
+        Student st2 = new Student("Nikolay", 'm' , 28, 2, 6.4);
+        Student st3 = new Student("Elana", 'f' , 19, 1, 8.9);
+        Student st4 = new Student("Petr", 'm' , 35, 4, 7);
+        Student st5 = new Student("Mariya", 'f' , 23, 3, 9.1);
+        ArrayList<Student> students = new ArrayList<>();
+        students.add(st1);
+        students.add(st2);
+        students.add(st3);
+        students.add(st4);
+        students.add(st5);
+
+        List<Student> collect = students.stream().filter(student -> student.age > 22 && student.avgGrade < 7.2).toList();
+        System.out.println(collect);
+    }
+}
+
+
+class Student {
+    String name;
+    char sex;
+    int age;
+    int course;
+    double avgGrade;
+
+    public Student(String name, char sex, int age, int course, double avgGrade) {
+        this.name = name;
+        this.sex = sex;
+        this.age = age;
+        this.course = course;
+        this.avgGrade = avgGrade;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", sex=" + sex +
+                ", age=" + age +
+                ", course=" + course +
+                ", avgGrade=" + avgGrade +
+                '}';
+    }
+}
